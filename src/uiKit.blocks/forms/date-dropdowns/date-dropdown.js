c5ee0b6dcf10025dd81bdb1476e1191d542670a7
Grigory <Neillias@users.noreply.github.com>
2019-10-12 17:45:46 +0300

    some fixes
    Некоторые исправления функционала кнопок "принять"

--- src/uiKit.blocks/forms/date-dropdowns/date-dropdown.js
+++ src/uiKit.blocks/forms/date-dropdowns/date-dropdown.js
@@ -19,5 +19,37 @@ export default function dateDropdown () {
                 }
             }            
         }
+
+        if (e.target && e.target.className == 'date-dropdown__apply-btn') {
+            var dateContainer = e.target,
+                rotated;
+            
+            while (!dateContainer.classList.contains('date-dropdowns')){
+                dateContainer = dateContainer.parentNode;
+                
+                if (dateContainer.classList.contains('date-dropdowns')){
+                    for (let i = 0; i < dateContainer.children.length; i++) {
+                        var thisChildren = dateContainer.children[i];                    
+                        
+                        if (thisChildren.classList.contains('date-dropdowns__input') || thisChildren.classList.contains('date-dropdowns__input_type_filterDate')) {
+                            var thisToggle = thisChildren.querySelector('.date-dropdown__toggle').firstChild;
+                            if (thisToggle.style.transform == 'rotate(180deg)'){
+                                rotated = true;
+                            } else if (thisToggle.style.transform == 'rotate(0deg)'){
+                                rotated = false;
+                            }
+            
+                            if (rotated) {
+                                thisToggle.style.transform = 'rotate(0)';
+                            }
+                        }
+                        
+                        if (thisChildren.classList.contains('date-dropdown__datepick-container')){
+                            thisChildren.style.display = (dateContainer.children[i].style.display == 'block') ? 'none' : 'block'
+                        }                           
+                    }
+                }                
+            }            
+        } 
     });
 }
--- src/uiKit.blocks/forms/dropdown/dropdown.js
+++ src/uiKit.blocks/forms/dropdown/dropdown.js
@@ -14,5 +14,26 @@ export default function dropdown () {
                 e.target.style.transform = 'rotate('+0+'deg)';
             };
         }
+
+        if (e.target && e.target.classList.contains('dropdown__apply-btn')) {
+            var dateContainer = e.target;
+            
+            while (!dateContainer.classList.contains('dropdown')) {
+                dateContainer = dateContainer.parentNode;
+                
+                if (dateContainer.classList.contains('dropdown')) {
+                    
+                    for (let i = 0; i < dateContainer.children.length; i++) {                        
+                        if (dateContainer.children[i].classList.contains('dropdown__menu-container') && dateContainer.children[i].classList.contains('dropped')){
+                            dateContainer.children[i].classList.remove('dropped');
+                            dateContainer.children[i].classList.add('hidden');
+                        }
+                        if (dateContainer.children[i].classList.contains('dropdown__menu-button')){
+                            dateContainer.children[i].style.transform = 'rotate(0)';
+                        }
+                    }
+                }
+            }
+        }
     });
 };
